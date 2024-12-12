8b70166bb977920663d043963eb25e02081e7f5f
Priya Dharsini P <dharsini15306@gmail.com>
2024-12-13 00:34:26 +0530

    extra lines

--- src/scripts/generate-qrcodes.js
+++ src/scripts/generate-qrcodes.js
@@ -1,19 +1,4 @@
-// import { generateQRCodes } from '../utils/qrGenerator.js';
 
-// async function initializeQRCodes() {
-//   try {
-//     const hints = await generateQRCodes();
-//     console.log('QR Codes generation process completed');
-//     console.log('Hints:', hints);
-//     process.exit(0);
-//   } catch (error) {
-//     console.error('Error generating QR codes:', error);
-//     process.exit(1);
-//   }
-// }
-
-// initializeQRCodes();
-// Change 'import' to 'require' for CommonJS
 const { generateQRCodes } = require('../utils/qrGenerator.js');
 
 async function initializeQRCodes() {
